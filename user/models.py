78a214230aae5e4611c4f24c1e56c61d9d6c3eaf
Ostap <ostapbojko04@gmail.com>
2024-08-13 11:00:52 +0300

    fix with Pillow

--- user/models.py
+++ user/models.py
@@ -1,8 +1,8 @@
+from django.db import models
 from django.contrib.auth.models import (
     AbstractUser,
     UserManager as DjangoUserManager
 )
-from django.db import models
 from django.utils.translation import gettext as _
 
 
