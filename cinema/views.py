6ae8ec3cdf20218851c3443c89f50acaed257d3c
Ostap <ostapbojko04@gmail.com>
2024-08-13 10:54:12 +0300

    commit1

--- cinema/views.py
+++ cinema/views.py
@@ -1,11 +1,16 @@
 from datetime import datetime
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, mixins, status
 
 from django.db.models import F, Count
 from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
 from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
@@ -22,6 +27,7 @@ from cinema.serializers import (
     MovieListSerializer,
     OrderSerializer,
     OrderListSerializer,
+    MovieImageSerializer
 )
 
 
@@ -66,7 +72,7 @@ class MovieViewSet(
     queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+    permission_classes = ()
 
     @staticmethod
     def _params_to_ints(qs):
@@ -101,7 +107,21 @@ class MovieViewSet(
         if self.action == "retrieve":
             return MovieDetailSerializer
 
+        if self.action == "upload_image":
+            return MovieImageSerializer
+
         return MovieSerializer
+
+    @action(methods=["POST"], detail=True, url_path="upload-image", permission_classes=[IsAdminUser])
+    def upload_image(self, request, pk=None):
+        item = self.get_object()
+        serializer = self.get_serializer(item, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
